eb0ba8e4a5a0bc116c1322dbf4210c1eef27428f
FRC Team Blitz #2083 <github@teamblitz.net>
2019-10-08 19:06:50 -0600

    Final code changes from 2019

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -54,7 +54,8 @@ public class RobotMap {
 	public static TalonSRX frontLeftMotor = new TalonSRX(2); // 2
 	static {
 		frontLeftMotor.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 0, 10);
-		frontLeftMotor.configPeakCurrentLimit(12);
+		frontLeftMotor.configPeakCurrentLimit(40);
+		//frontLeftMotor.enableCurrentLimit(true);
 		frontLeftMotor.setSensorPhase(true); //Positive velocity corresponds to green light on Talon
 		frontLeftMotor.setInverted(false);
 		frontLeftMotor.configNominalOutputForward(0, 10);
@@ -80,7 +81,8 @@ public class RobotMap {
 	public static TalonSRX frontRightMotor = new TalonSRX(1); // 1
 	static {
 		frontRightMotor.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 0, 10);
-		frontRightMotor.configPeakCurrentLimit(12); //Peak current
+		frontRightMotor.configPeakCurrentLimit(40); //Peak current
+		//frontRightMotor.enableCurrentLimit(true);
 		frontRightMotor.setSensorPhase(true);
 		frontRightMotor.setInverted(false);
 		frontRightMotor.configNominalOutputForward(0, 10);
